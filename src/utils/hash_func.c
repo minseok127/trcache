682ccc805d2771cb322f452d956d777cb92a180d
minseok127 <minseok0yoon@gmail.com>
2025-04-27 16:10:31 +0900

    add hashfunc source

--- src/utils/hash_func.c
+++ src/utils/hash_func.c
@@ -1,39 +1,49 @@
-uint32_t murmur3_32(const void *key, size_t len, uint32_t seed)
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "utils/hash_func.h"
+
+uint64_t murmur_hash(const void *key, size_t len, uint32_t seed)
 {
+	const uint64_t m = 0xc6a4a7935bd1e995ULL;
+	const int r = 47;
+
+	uint64_t h = seed ^ (len * m);
+
 	const uint8_t *data = (const uint8_t *)key;
-	const int nblocks  = (int)(len / 4);
-	uint32_t h1 = seed;
-	const uint32_t c1 = 0xcc9e2d51;
-	const uint32_t c2 = 0x1b873593;
+	const uint8_t *end = data + len;
 
-	const uint32_t *blocks = (const uint32_t *)data;
-	for (int i = 0; i < nblocks; ++i) {
-		uint32_t k1 = blocks[i];
-		k1 *= c1;
-		k1 = (k1 << 15) | (k1 >> 17);
-		k1 *= c2;
-		h1 ^= k1;
-		h1 = (h1 << 13) | (h1 >> 19);
-		h1 = h1 * 5 + 0xe6546b64;
+	while (data + 8 <= end) {
+		uint64_t k;
+		memcpy(&k, data, sizeof(k));
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h ^= k;
+		h *= m;
+
+		data += 8;
 	}
 
-	const uint8_t *tail = data + nblocks * 4;
-	uint32_t k1 = 0;
-	switch (len & 3) {
-		case 3: k1 ^= tail[2] << 16;
-		case 2: k1 ^= tail[1] << 8;
-		case 1: k1 ^= tail[0];
-				k1 *= c1;
-				k1 = (k1 << 15) | (k1 >> 17);
-				k1 *= c2;
-				h1 ^= k1;
+	uint64_t tail = 0;
+	switch (end - data) {
+		case 7: tail ^= (uint64_t)data[6] << 48;
+		case 6: tail ^= (uint64_t)data[5] << 40;
+		case 5: tail ^= (uint64_t)data[4] << 32;
+		case 4: tail ^= (uint64_t)data[3] << 24;
+		case 3: tail ^= (uint64_t)data[2] << 16;
+		case 2: tail ^= (uint64_t)data[1] <<  8;
+		case 1: tail ^= (uint64_t)data[0];
+				tail *= m;
+				h ^= tail;
 	}
 
-	h1 ^= (uint32_t)len;
-	h1 ^= h1 >> 16;
-	h1 *= 0x85ebca6b;
-	h1 ^= h1 >> 13;
-	h1 *= 0xc2b2ae35;
-	h1 ^= h1 >> 16;
-	return h1;
+	h ^= h >> r;
+	h *= m;
+	h ^= h >> r;
+
+	return h;
 }
