ea672620da42764d9b1bed0996e3575b56ffa500
minseok127 <minseok0yoon@gmail.com>
2025-04-27 16:12:46 +0900

    create symbol table header

---
+++ src/include/core/symbol_table.h
@@ -0,0 +1,12 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+struct trc_admin_symbol_entry {
+
+};
+
+struct trc_public_symbol_entry {
+
+};
+
+#endif /* SYMBOL_TABLE_H */
--- src/include/core/trcache_internal.h
+++ src/include/core/trcache_internal.h
@@ -1,15 +1,5 @@
 #ifndef TRCACHE_INTERNAL_H
 #define TRCACHE_INTERNAL_H
 
-struct trcache_symbol_entry {
-	pthread_spinlock_t spinlock;
-	int num_worker_thread;
-};
-
-struct trcache {
-	struct trcache_symbol_entry symbol_entry_table[TRCACHE_MAX_SYMBOL_COUNT];
-	int num_symbol_entry;
-	int trcache_id;
-};
 
 #endif /* TRCACHE_INTERNAL_H */
