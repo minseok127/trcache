32ed69942b070a5bfd2cea34d819d674706908f1
minseok127 <minseok0yoon@gmail.com>
2025-06-04 15:31:49 +0900

    Fix alignment of comments

--- src/include/concurrent/scalable_queue.h
+++ src/include/concurrent/scalable_queue.h
@@ -3,13 +3,7 @@
 
 #include <stddef.h>
 #include <stdbool.h>
-
 #include <stdint.h>
-
-/**
- * @file   concurrent/scalable_queue.h
- * @brief  Scalable multi-producer multi-consumer queue.
- */
 
 typedef struct scalable_queue scalable_queue;
 
@@ -17,28 +11,32 @@ typedef struct scalable_queue scalable_queue;
 typedef struct scalable_queue scq;
 
 /**
- * @brief Create a new scalable_queue instance.
+ * @brief   Create a new scalable_queue instance.
  *
- * @return Pointer to queue on success, NULL on failure.
+ * @return  Pointer to queue on success, NULL on failure.
  */
 struct scalable_queue *scq_init(void);
 
 /**
- * @brief Destroy a scalable_queue and free all associated memory.
+ * @brief   Destroy a scalable_queue and free all associated memory.
  */
 void scq_destroy(struct scalable_queue *scq);
 
 /**
- * @brief Enqueue a datum into the queue.
+ * @brief   Enqueue a datum into the queue.
+ *
+ * @param   scq:   Queue instance.
+ * @param   datum: Pointer of scalar.
  */
 void scq_enqueue(struct scalable_queue *scq, void *datum);
 
 /**
- * @brief Dequeue a datum from the queue.
+ * @brief   Dequeue a datum from the queue.
  *
  * @param   scq:   Queue instance.
  * @param   datum: Output pointer to store dequeued datum.
- * @return true if an element was dequeued.
+ *
+ * @return  true if an element was dequeued.
  */
 bool scq_dequeue(struct scalable_queue *scq, void **datum);
 
