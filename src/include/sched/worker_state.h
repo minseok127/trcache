df6c311199bff6a4d762148dda0fd54401183499
minseok127 <minseok0yoon@gmail.com>
2025-06-04 16:26:20 +0900

    Add worker state allocation

--- src/include/sched/worker_state.h
+++ src/include/sched/worker_state.h
@@ -3,9 +3,15 @@
 
 #include "sched/worker_stat_board.h"
 
+/**
+ * worker_state - Per-worker runtime data.
+ *
+ * @worker_id: Numeric ID assigned to the worker thread.
+ * @stat:      Performance counters split per pipeline stage.
+ */
 struct worker_state {
-	int worker_id;
-	struct worker_stat_board stat;
+        int worker_id;
+        struct worker_stat_board stat;
 };
 
 #endif /* WORKER_STATE_H */
