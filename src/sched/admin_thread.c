8695ba4ce76620b45780346171544dde08191f26
minseok127 <minseok0yoon@gmail.com>
2025-06-10 12:44:30 +0900

    Update admin_thread.c

--- src/sched/admin_thread.c
+++ src/sched/admin_thread.c
@@ -3,6 +3,7 @@
  * @brief  Implementation of the admin thread main routine.
  */
 #define _GNU_SOURCE
+#include <sched.h>
 
 #include "sched/admin_thread.h"
 #include "meta/symbol_table.h"
@@ -11,7 +12,6 @@
 #include "concurrent/atomsnap.h"
 #include "utils/log.h"
 #include "utils/tsc_clock.h"
-#include <sched.h>
 
 /**
  * @brief   Initialise the admin thread state.
@@ -185,17 +185,17 @@ static void post_work_msg(struct trcache *cache, int worker_id,
  */
 static int choose_best_worker(double *load, int limit)
 {
-int best = 0;
-double min_load = load[0];
+	int best = 0;
+	double min_load = load[0];
 
-for (int w = 1; w < limit; w++) {
-if (load[w] < min_load) {
-min_load = load[w];
-best = w;
-}
-}
+	for (int w = 1; w < limit; w++) {
+		if (load[w] < min_load) {
+			min_load = load[w];
+			best = w;
+		}
+	}
 
-return best;
+	return best;
 }
 
 struct stage_sched_env {
@@ -203,67 +203,71 @@ struct stage_sched_env {
 	double *load;
 	int limit;
 	int start;
-	};
+};
 
 static void update_stage_assignment(struct trcache *cache,
-struct symbol_entry *entry, int idx,
-trcache_candle_type type, struct stage_sched_env *env,
-int worker)
+	struct symbol_entry *entry, int idx,
+	trcache_candle_type type, struct stage_sched_env *env,
+	int worker)
 {
 	int cur = atomic_load(&entry->in_progress[env->stage][idx]);
-	if (cur == worker)
-	return;
-	
-	if (cur >= 0)
-	post_work_msg(cache, cur, type, env->stage,
-	entry->id, SCHED_MSG_REMOVE_WORK);
-	
-	post_work_msg(cache, worker, type, env->stage,
-	entry->id, SCHED_MSG_ADD_WORK);
+	if (cur == worker) {
+		return;
 	}
+		
+	if (cur >= 0) {
+		post_work_msg(cache, cur, type, env->stage,
+			entry->id, SCHED_MSG_REMOVE_WORK);
+	}
+		
+	post_work_msg(cache, worker, type, env->stage,
+		entry->id, SCHED_MSG_ADD_WORK);
+}
 	
-	static void schedule_symbol_stage(struct trcache *cache,
+static void schedule_symbol_stage(struct trcache *cache,
 	struct symbol_entry *entry, int idx,
 	trcache_candle_type type, double demand,
 	struct stage_sched_env *env)
-	{
-	if (env->limit <= 0)
-	return;
-	
+{
+	if (env->limit <= 0) {
+		return;
+	}
+		
 	int best = choose_best_worker(env->load, env->limit) + env->start;
 	env->load[best - env->start] += demand;
 	
 	update_stage_assignment(cache, entry, idx, type, env, best);
-	}
+}
 	
-	static void schedule_symbol_work(struct trcache *cache,
+static void schedule_symbol_work(struct trcache *cache,
 	struct symbol_entry *entry, double load[][MAX_NUM_THREADS],
 	const int *stage_limits, const int *stage_start)
-	{
+{
 	struct stage_sched_env env[WORKER_STAT_STAGE_NUM];
 	trcache_candle_type_flags flags = cache->candle_type_flags;
 	
 	for (int s = 0; s < WORKER_STAT_STAGE_NUM; s++) {
-	env[s].stage = s;
-	env[s].load = load[s];
-	env[s].limit = stage_limits[s];
-	env[s].start = stage_start[s];
+		env[s].stage = s;
+		env[s].load = load[s];
+		env[s].limit = stage_limits[s];
+		env[s].start = stage_start[s];
 	}
 	
 	for (uint32_t m = flags; m != 0; m &= m - 1) {
-	int idx = __builtin_ctz(m);
-	trcache_candle_type t = 1u << idx;
-	struct sched_stage_rate *r =
-	&entry->pipeline_stats.stage_rates[idx];
-	double demand[WORKER_STAT_STAGE_NUM] = {
-	(double)r->produced_rate + 1.0,
-	(double)r->completed_rate + 1.0,
-	(double)r->converted_rate + 1.0,
-	};
+		int idx = __builtin_ctz(m);
+		trcache_candle_type t = 1u << idx;
+		struct sched_stage_rate *r =
+			&entry->pipeline_stats.stage_rates[idx];
+		double demand[WORKER_STAT_STAGE_NUM] = {
+			(double)r->produced_rate + 1.0,
+			(double)r->completed_rate + 1.0,
+			(double)r->converted_rate + 1.0,
+		};
 	
-	for (int s = 0; s < WORKER_STAT_STAGE_NUM; s++)
-	schedule_symbol_stage(cache, entry, idx, t,
-	demand[s], &env[s]);
+		for (int s = 0; s < WORKER_STAT_STAGE_NUM; s++) {
+			schedule_symbol_stage(cache, entry, idx, t,
+				demand[s], &env[s]);
+		}
 	}
 }
 
@@ -313,31 +317,31 @@ static void compute_stage_limits(struct trcache *cache, int *limits)
  */
 static void balance_workers(struct trcache *cache)
 {
-        struct symbol_table *table = cache->symbol_table;
-        struct atomsnap_version *ver = NULL;
-        struct symbol_entry **arr = NULL;
-        int num = 0;
-        int limits[WORKER_STAT_STAGE_NUM];
-        int stage_start[WORKER_STAT_STAGE_NUM];
-        double load[WORKER_STAT_STAGE_NUM][MAX_NUM_THREADS] = { { 0 } };
+	struct symbol_table *table = cache->symbol_table;
+	struct atomsnap_version *ver = NULL;
+	struct symbol_entry **arr = NULL;
+	int num = 0;
+	int limits[WORKER_STAT_STAGE_NUM];
+	int stage_start[WORKER_STAT_STAGE_NUM];
+	double load[WORKER_STAT_STAGE_NUM][MAX_NUM_THREADS] = { { 0 } };
 
-        compute_stage_limits(cache, limits);
+	compute_stage_limits(cache, limits);
 
-        stage_start[WORKER_STAT_STAGE_APPLY] = 0;
-        stage_start[WORKER_STAT_STAGE_CONVERT] =
-                stage_start[WORKER_STAT_STAGE_APPLY] + limits[WORKER_STAT_STAGE_APPLY];
-        stage_start[WORKER_STAT_STAGE_FLUSH] =
-                stage_start[WORKER_STAT_STAGE_CONVERT] + limits[WORKER_STAT_STAGE_CONVERT];
+	stage_start[WORKER_STAT_STAGE_APPLY] = 0;
+	stage_start[WORKER_STAT_STAGE_CONVERT] =
+		stage_start[WORKER_STAT_STAGE_APPLY] + limits[WORKER_STAT_STAGE_APPLY];
+	stage_start[WORKER_STAT_STAGE_FLUSH] =
+    	stage_start[WORKER_STAT_STAGE_CONVERT] + limits[WORKER_STAT_STAGE_CONVERT];
 
-        ver = atomsnap_acquire_version(table->symbol_ptr_array_gate);
-        arr = (struct symbol_entry **)ver->object;
-        num = table->num_symbols;
+	ver = atomsnap_acquire_version(table->symbol_ptr_array_gate);
+	arr = (struct symbol_entry **)ver->object;
+	num = table->num_symbols;
 
-        for (int i = 0; i < num; i++) {
-                schedule_symbol_work(cache, arr[i], load, limits, stage_start);
-        }
+	for (int i = 0; i < num; i++) {
+		schedule_symbol_work(cache, arr[i], load, limits, stage_start);
+	}
 
-        atomsnap_release_version(ver);
+	atomsnap_release_version(ver);
 }
 
 /**
