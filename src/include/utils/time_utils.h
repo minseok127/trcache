e21f7cb757439cde8614fcedf6a456ebeb7417be
minseok127 <minseok0yoon@gmail.com>
2025-06-18 10:22:59 +0900

    Update time_utils.h

--- src/include/utils/time_utils.h
+++ src/include/utils/time_utils.h
@@ -8,15 +8,15 @@
 
 static inline void format_timestamp_ms(uint64_t ts_ms, char *buf, size_t buf_len)
 {
-    time_t secs = (time_t)(ts_ms / 1000ULL);
-    int ms = (int)(ts_ms % 1000ULL);
-    struct tm tm;
-    gmtime_r(&secs, &tm);
-    strftime(buf, buf_len, "%Y-%m-%d %H:%M:%S", &tm);
-    size_t len = strlen(buf);
-    if (len < buf_len) {
-        snprintf(buf + len, buf_len - len, ".%03d", ms);
-    }
+	time_t secs = (time_t)(ts_ms / 1000ULL);
+	int ms = (int)(ts_ms % 1000ULL);
+	struct tm tm;
+	gmtime_r(&secs, &tm);
+	strftime(buf, buf_len, "%Y-%m-%d %H:%M:%S", &tm);
+	size_t len = strlen(buf);
+	if (len < buf_len) {
+		snprintf(buf + len, buf_len - len, ".%03d", ms);
+	}
 }
 
 #endif /* TIME_UTILS_H */
